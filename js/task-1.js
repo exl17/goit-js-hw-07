7a1f4aa125966c6af6d3cf6efc0a5632a7836704
exl17 <143128264+exl17@users.noreply.github.com>
2023-12-29 09:20:53 +0200

    homework-7

---
+++ js/task-1.js
@@ -0,0 +1,8 @@
+const listWithId = document.querySelectorAll('#categories');
+console.log(listWithId);
+
+const categoriesItemsByTagName = document.querySelectorAll("li");
+console.log(categoriesItemsByTagName);
+
+const categoriesItemsByClass = document.querySelectorAll(".item");
+console.log(categoriesItemsByClass);
