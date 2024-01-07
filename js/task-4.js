0e292924c4ce3d15a57c6a40a1dbc2719ab6fdf9
exl17 <143128264+exl17@users.noreply.github.com>
2024-01-08 00:34:24 +0200

    homework-7(3)

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  
+  if (email === "" || password === "") {
+    alert ("All form fields must be filled in");
+  }
+    
+     const formData = {
+      email: email,
+      password: '*'.repeat(password.length)
+    };
+
+  console.log(formData);
+  
+  form.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,15 @@
+const button = document.querySelector(".change-color");
+const widget = document.querySelector(".widget");
+
+button.addEventListener('click', function () {
+const newColor = getRandomHexColor();
+widget.style.backgroundColor = newColor;
+
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
